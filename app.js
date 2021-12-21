7658de74b3ea1f8a1b9e795c04bc71781ad27146
mouad aem <mouad.aitelmahjoub@gmail.com>
2021-12-21 13:38:17 +0100

    Db Connection ✔

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
 
-const app = express();
+const app = express()
+dotenv.config()
 
 // middleware
-app.use(express.static('public'));
+app.use(express.static("public"))
 
 // view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs")
 
 // database connection
-const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+const DB_URI = process.env.DB_URI
+mongoose
+  .connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log(err))
 
 // routes
-app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get("/", (req, res) => res.render("home"))
+app.get("/smoothies", (req, res) => res.render("smoothies"))
